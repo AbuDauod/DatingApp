f4e78e171bb8b05083bf6199a9cb40d600436c12
AbuDauod <Tareq.da1983@gmail.com>
2025-01-22 13:07:08 +0300

    End of Section 4

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
-    public DbSet<AppUser>? Users { get; set; }
+    public DbSet<AppUser> Users { get; set; }
 }
 
 
